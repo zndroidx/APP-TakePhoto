6ede51c56fd1c626de35c17ecc767c7309a7c842
lazy <luzhenyuxfcy@sina.com>
2020-04-09 11:21:24 +0800

    add function demo for user of androidx

--- app/src/main/java/com/app/MainActivity.java
+++ app/src/main/java/com/app/MainActivity.java
@@ -1,14 +1,119 @@
 package com.app;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.zndroid.takephoto.CallBack;
+import com.zndroid.takephoto.TakePhotoAPI;
+
+import org.devio.takephoto.model.CropOptions;
+import org.devio.takephoto.model.InvokeParam;
+import org.devio.takephoto.model.TResult;
+import org.devio.takephoto.permission.PermissionManager;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TakePhotoAPI api;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.im_show);
+
+        api = new TakePhotoAPI();
+        api.getDefault(this, new CallBack() {
+            @Override
+            public void takeSuccess(final TResult result) {
+                Toast.makeText(MainActivity.this, "成功选择了" + result.getImages().size() + "张", Toast.LENGTH_LONG).show();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        InputStream inputStream = null;
+                        try {
+                            inputStream = new FileInputStream(result.getImage().getCompressPath());
+                            Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                            imageView.setImageBitmap(bitmap);
+                        } catch (FileNotFoundException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                });
+            }
+
+            @Override
+            public void takeFail(TResult result, String msg) {
+                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void takeCancel() {
+                Toast.makeText(MainActivity.this, "取消", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public PermissionManager.TPermissionType invoke(InvokeParam invokeParam) {
+                return api.invoke(invokeParam, MainActivity.this);
+            }
+        });
+        api.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        api.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        api.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        api.onRequestPermissionsResult(this, requestCode, permissions, grantResults);
+    }
+
+    public void take_photo(View view) {
+        if (api != null)
+            api.openCamera();
+    }
+
+    public void open_gallery(View view) {
+        if (api != null)
+            api.openGallery();
+    }
+
+    public void open_gallery_mux(View view) {
+        if (api != null)
+            api.openGallery(3);
+    }
+
+    public void take_photo_with_crop(View view) {
+        if (api != null) {
+            CropOptions cropOptions = new CropOptions.Builder()
+                    .setWithOwnCrop(true)
+                    .create();
+            api.openCameraWithCrop(cropOptions);
+        }
     }
 }
---
+++ lib_takephoto/src/main/java/com/zndroid/takephoto/TakePhotoAPI.java
@@ -0,0 +1,201 @@
+package com.zndroid.takephoto;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import org.devio.takephoto.app.TakePhoto;
+import org.devio.takephoto.app.TakePhotoImpl;
+import org.devio.takephoto.compress.CompressConfig;
+import org.devio.takephoto.model.CropOptions;
+import org.devio.takephoto.model.InvokeParam;
+import org.devio.takephoto.model.TContextWrap;
+import org.devio.takephoto.permission.PermissionManager;
+import org.devio.takephoto.permission.TakePhotoInvocationHandler;
+
+import java.io.File;
+
+/**
+ * @name:TakePhotoAPI
+ * @author:lazy
+ * @email:luzhenyuxfcy@sina.com
+ * @date : 2020/4/8 22:52
+ * @version:
+ * @description:TODO
+ */
+public class TakePhotoAPI extends API<TakePhotoAPI> {
+    private TakePhoto takePhoto;
+    private InvokeParam invokeParam;
+    private CallBack callBack;
+
+    @Override
+    public TakePhotoAPI getDefault(Activity activity, CallBack callBack) {
+        this.callBack = callBack;
+
+        if (takePhoto == null) {
+            takePhoto = (TakePhoto) TakePhotoInvocationHandler.of(callBack).bind(new TakePhotoImpl(activity, callBack));
+        }
+        return this;
+    }
+
+    @Override
+    protected String getTag() {
+        return "TakePhotoAPI";
+    }
+
+    public PermissionManager.TPermissionType invoke(InvokeParam invokeParam, Activity activity) {
+        PermissionManager.TPermissionType type = PermissionManager.checkPermission(TContextWrap.of(activity), invokeParam.getMethod());
+        if (PermissionManager.TPermissionType.WAIT.equals(type)) {
+            this.invokeParam = invokeParam;
+        }
+        return type;
+    }
+
+    /**
+     * 获取TakePhoto主入口
+     * */
+    public TakePhoto getTakePhoto() {
+        return takePhoto;
+    }
+
+    /**
+     * 打开相册
+     * */
+    public void openGallery() {
+        try {
+            showLog("openGallery");
+            openSysGallery();
+        } catch (Exception e) {
+            showError(e.getMessage());
+        }
+    }
+
+    /**
+     * 打开相册支持多选
+     * @param limit 最多支持选择张数
+     * */
+    public void openGallery(int limit) {
+        try {
+            showLog("openGallery limit=" + limit);
+            openSysGallery(limit);
+        } catch (Exception e) {
+            showError(e.getMessage());
+        }
+    }
+
+    /**
+     * 打开相机并拍照
+     * */
+    public void openCamera() {
+        try {
+            showLog("openCamera");
+            openSysCamera();
+        } catch (Exception e) {
+            showError(e.getMessage());
+        }
+    }
+
+    /**
+     * 打开相机并拍照，支持裁剪
+     * */
+    public void openCameraWithCrop(CropOptions cropOptions) {
+        try {
+            showLog("openCameraWithCrop");
+            openSysCameraWithCrop(cropOptions);
+        } catch (Exception e) {
+            showError(e.getMessage());
+        }
+    }
+
+    private void configCompress(TakePhoto takePhoto) {
+        int maxSize = Integer.parseInt("10240");
+        int width = Integer.parseInt("400");
+        int height = Integer.parseInt("400");
+        boolean showProgressBar = true;
+        boolean enableRawFile = false;
+        CompressConfig config = new CompressConfig.Builder().setMaxSize(maxSize)
+                .setMaxPixel(width >= height ? width : height)
+                .enableReserveRaw(enableRawFile)
+                .create();
+        takePhoto.onEnableCompress(config, showProgressBar);
+    }
+
+    private void openSysGallery() {
+        if (takePhoto == null) showError("'takePhoto is null'");
+
+        configCompress(takePhoto);
+        takePhoto.onPickFromGallery();
+    }
+
+    private void openSysGallery(int limit) {
+        if (takePhoto == null) showError("'takePhoto is null'");
+
+        configCompress(takePhoto);
+        if (limit <= 0) limit = 1;
+        takePhoto.onPickMultiple(limit);
+    }
+
+    /**
+     * 打开系统相机
+     */
+    private void openSysCamera() {
+        if (takePhoto == null) showError("'takePhoto is null'");
+
+        File file = new File(Environment.getExternalStorageDirectory(), "/temp/" + System.currentTimeMillis() + ".jpg");
+        if (!file.getParentFile().exists()) {
+            file.getParentFile().mkdirs();
+        }
+        Uri imageUri = Uri.fromFile(file);
+
+        configCompress(takePhoto);
+        takePhoto.onPickFromCapture(imageUri);
+    }
+
+    private void openSysCameraWithCrop(CropOptions cropOptions) {
+        if (takePhoto == null || cropOptions == null) showError("'takePhoto is null'");
+
+        File file = new File(Environment.getExternalStorageDirectory(), "/temp/" + System.currentTimeMillis() + ".jpg");
+        if (!file.getParentFile().exists()) {
+            file.getParentFile().mkdirs();
+        }
+        Uri imageUri = Uri.fromFile(file);
+
+        configCompress(takePhoto);
+
+        takePhoto.onPickFromCaptureWithCrop(imageUri, cropOptions);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (takePhoto != null)
+            takePhoto.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (takePhoto != null)
+            takePhoto.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(Activity activity, int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        PermissionManager.TPermissionType type = PermissionManager.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (null != callBack)
+            PermissionManager.handlePermissionsResult(activity, type, invokeParam, callBack);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (takePhoto != null)
+            takePhoto.onActivityResult(requestCode, resultCode, data);
+    }
+}
